03100c77752c3afbb4110cf6088465f75d08f9bc
ual-laptop <praveenrnair49@gmail.com>
2022-10-31 03:15:29 -0700

    third

--- hmsproject/settings.py
+++ hmsproject/settings.py
@@ -129,16 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-LOGIN_REDIRECT_URL = 'home'
-
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-
-MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
